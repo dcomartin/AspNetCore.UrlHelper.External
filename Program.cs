3261f8295bfdbd2670fa067944c456c92c760b8a
Derek Comartin <dcomartin@gmail.com>
2018-08-08 21:41:52 -0400

    Initial Demo

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Demo
+{           
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args);
+        }
+
+        private static IWebHost BuildWebHost(string[] args)
+        {
+            var webhost = WebHost.CreateDefaultBuilder(args)
+                .SniffRouteData()
+                .UseStartup<Startup>()
+                .Build();
+    
+            var urlHelper = webhost.GetStaticUrlHelper("https://my.internal.service:48923/somepath");
+            Console.WriteLine(urlHelper.Link(nameof(ValuesController), null));
+            return webhost;
+        }
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo
+{
+    [Route("api/[controller]", Name = nameof(ValuesController))]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
